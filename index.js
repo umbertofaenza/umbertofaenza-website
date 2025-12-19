b91b0d499e9e7382aa816e5267566b6e3d83fdef
umbertofaenza <umberto.faenza@gmail.com>
2025-12-19 12:13:45 +0100

    handle bg opacity based on modal attribute, general optimization

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Dinamically populate copyright year
-  const footerCopyrightYear = document.querySelector(
-    ".copyright .current-year"
-  );
-  footerCopyrightYear.innerHTML = new Date().getFullYear();
+  document.querySelector(".copyright .current-year").innerHTML =
+    new Date().getFullYear();
 
-  // Handle open-close modal logic + background opacity
-  const policyLink = document.querySelector(".policy-link");
-  const policyModal = document.querySelector("#policyModal");
-  const closePolicy = document.querySelector(".close-policy");
-
-  policyLink.addEventListener("click", function () {
-    document.querySelector("body").style.opacity = 0.2;
-    policyModal.showModal();
-  });
-
-  closePolicy.addEventListener("click", function () {
-    policyModal.close();
-    document.querySelector("body").style.opacity = 1;
-  });
+  // Handle background opacity based on opened-closed modal
+  document.querySelector("#policyModal").attributes.open
+    ? (document.querySelector("body").style.opacity = 0.2)
+    : (document.querySelector("body").style.opacity = 1);
 });
